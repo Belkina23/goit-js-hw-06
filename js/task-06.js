1a19b6cb5e660d7450afd92192c4bdc55a3c5cce
Belkina23 <23zakat@gmail.com>
2022-11-02 15:09:13 +0200

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,46 @@
+// Напиши скрипт, который при потере фокуса на инпуте (событие blur), 
+// проверяет его содержимое на правильное количество введённых символов.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Please enter 6 symbols"
+// />
+
+// Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
+// Если введено подходящее количество символов, то border инпута становится зелёным, 
+// если неправильное - красным.
+// Для добавления стилей, используй CSS-классы valid и invalid, которые мы уже добавили 
+// в исходные файлы задания.
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+const inputElement = document.querySelector('#validation-input');
+inputElement.addEventListener('blur', inputBlur);
+
+function inputBlur () {
+    if (
+        inputElement.value.length === Number(inputElement.dataset.length)
+      ) {
+        if (inputElement.classList.contains('invalid')) {
+            inputElement.classList.remove('invalid');
+        }
+        inputElement.classList.add('valid');
+      } else {
+        if (inputElement.classList.contains('valid')) {
+            inputElement.classList.remove('valid');
+        }
+        inputElement.classList.add('invalid');
+      }
+};
